6a99b8fa6b0b07cb87c7a9d6858bef2524dffa49
crazydj8 <akshataryan8@gmail.com>
2022-10-17 12:24:43 +0530

    update

---
+++ nodejs/newexample1/asyncread.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+fs.readFile("test.txt", function(err,data){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Asynchronous read: " + data.toString());
+    }
+});
--- nodejs/newexample1/buffer.js
+++ nodejs/newexample1/buffer.js
@@ -1,8 +1,8 @@
-const buf1 = Buffer.alloc(10, '1');
-const buf2 = Buffer.alloc(17, '2');
+const buf1 = Buffer.alloc(8, "zi welcome");
+const buf2 = Buffer.alloc(10, "hi welcome2");
 
-console.log(buf1.toString());
-console.log(buf2.toString());
+//console.log(buf1.toString());
+//console.log(buf2.toString());
 
 var result = buf1.compare(buf2);
 
